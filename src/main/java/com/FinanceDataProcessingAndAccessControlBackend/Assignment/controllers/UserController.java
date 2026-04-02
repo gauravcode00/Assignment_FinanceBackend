8e0e25035a751742c54e325dfccd82f1edf23dcf
Gaurav <kumargaurav9817@gmail.com>
2026-04-02 14:17:58 +0530

    user registeration endpoint by permiting /api/users/register in SecurityConfig

---
+++ src/main/java/com/FinanceDataProcessingAndAccessControlBackend/Assignment/controllers/UserController.java
@@ -0,0 +1,31 @@
+package com.FinanceDataProcessingAndAccessControlBackend.Assignment.controllers;
+
+
+import com.FinanceDataProcessingAndAccessControlBackend.Assignment.dtos.UserRegistrationDto;
+import com.FinanceDataProcessingAndAccessControlBackend.Assignment.dtos.UserResponseDto;
+import com.FinanceDataProcessingAndAccessControlBackend.Assignment.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDto> registerUser(@RequestBody UserRegistrationDto registrationDto){
+
+        UserResponseDto createdUser = userService.registerUser(registrationDto); // calling userService
+
+        return  new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+}
